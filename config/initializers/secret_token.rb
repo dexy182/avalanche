f2a6c811e719b9499b72ff40cd65ad18fc570bc2
Meredith Underell <mer.underell@gmail.com>
2013-03-04 21:13:55 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avalanche::Application.config.secret_token = 'f2d7a485990b1f27a791bc281d0f9b310934e94001140465888393c1dc186bb82a5b5bae7f49a3da382bdc56511c3ca01d8f0df68be37b54bd1f4e122d438013'
